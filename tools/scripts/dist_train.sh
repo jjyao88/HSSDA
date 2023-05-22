f8bfd8d22779f004b8879717596c706199b7f74c
Tony Yao <11062997+jjyao88@users.noreply.github.com>
2023-05-22 10:53:41 +0800

    Update dist_train.sh

--- tools/scripts/dist_train.sh
+++ tools/scripts/dist_train.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
 
 set -x
-NGPUS=2
-PY_ARGS=$*  # ${@:2}
-
-export CUDA_VISIBLE_DEVICES=2,3
+NGPUS=$1
+PY_ARGS="${@:2}"
 
 python -m torch.distributed.launch --nproc_per_node=${NGPUS} train.py --launcher pytorch ${PY_ARGS}
